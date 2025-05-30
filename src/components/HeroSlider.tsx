ef2e4442a6ed3ddb6bc1fc51c639b67d12b13bdd
P0werK00kie <jw.breedlove@outlook.com>
2025-05-30 12:04:58 -0400

    Update Hero Overlay with 50% Black Background

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -5,7 +5,7 @@ const HeroSlider: React.FC = () => {
   return (
     <div className="hero-slider relative">
       <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: "url('https://wbuiebxqdjuxhvdtiata.supabase.co/storage/v1/object/sign/files/GECON/Gecon%20Main%20Website/Home%20Page/gecon_header_image.webp?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9jMjU3NzlmNC01ZjdjLTRjZWEtYmM2MS1mYjMwZjllNzkxM2EiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJmaWxlcy9HRUNPTi9HZWNvbiBNYWluIFdlYnNpdGUvSG9tZSBQYWdlL2dlY29uX2hlYWRlcl9pbWFnZS53ZWJwIiwiaWF0IjoxNzQ4NjIwOTkwLCJleHAiOjIzNzkzNDA5OTB9.pkyRLjC7hEISPaDWU9QMsDsLTWMKGjSIoUZum2RkUQU')" }}>
-        <div className="hero-overlay"></div>
+        <div className="hero-overlay absolute inset-0 bg-black bg-opacity-50"></div>
       </div>
       
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 relative h-full flex flex-col justify-center">
