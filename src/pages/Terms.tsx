faa902d1f79ab6f32e44cfbc2c8a9b399655ee76
P0werK00kie <jw.breedlove@outlook.com>
2025-06-02 13:03:07 -0400

    Updated Terms.tsx

--- src/pages/Terms.tsx
+++ src/pages/Terms.tsx
@@ -100,10 +100,10 @@ GECON grants You a limited, personal, nontransferable, nonsublicensable, revocab
 In addition to other restrictions set forth in these Terms of Use, You agree that:
                 </p>
                  <div className="bg-gray-50 p-6 rounded-lg">
-                  <p className="text-[#454d53]">You shall not disguise the origin of information transmitted through the Website.</p>
-                  <p className="text-[#454d53]">You will not place false or misleading information on the Website.</p>
-                  <p className="text-[#454d53]">You will not use or access any service, information, application or software available via the Website in a manner not expressly permitted by GECON.</p>
-                  <p className="text-[#454d53]">You will not input or upload to the Website any information which contains viruses, Trojan horses, worms, time bombs or other computer programming routines that are intended to damage, interfere with, intercept or expropriate any system, the Website or Information or that infringes the Intellectual Property (defined below) rights of another.</p>
+                  <p className="text-[#454d53]">- You shall not disguise the origin of information transmitted through the Website.</p>
+                  <p className="text-[#454d53]">- You will not place false or misleading information on the Website.</p>
+                  <p className="text-[#454d53]">- You will not use or access any service, information, application or software available via the Website in a manner not expressly permitted by GECON.</p>
+                  <p className="text-[#454d53]">- You will not input or upload to the Website any information which contains viruses, Trojan horses, worms, time bombs or other computer programming routines that are intended to damage, interfere with, intercept or expropriate any system, the Website or Information or that infringes the Intellectual Property (defined below) rights of another.</p>
                   <p className="text-[#454d53]">Phone: (678) 269-1114</p>
                 </div>
               </div>
