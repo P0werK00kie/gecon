903d3eb9ad09a2485a2155b8f352722fdc9ea2cf
P0werK00kie <jw.breedlove@outlook.com>
2025-06-03 15:15:46 -0400

    Implement SEO for Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SEO from '../components/SEO';
 import HeroSlider from '../components/HeroSlider';
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
@@ -9,6 +10,11 @@ import SubcontractorSection from '../components/SubcontractorSection';
 const Home = () => {
   return (
     <>
+      <SEO 
+        title="HOME"
+        description="Georgia Environmental Construction has dedicated thirty years to natural disaster recovery and response. When natural disasters strike, from hurricanes to tornadoes, Gecon provides emergency recovery and response to affected communities."
+        canonicalUrl="https://gecon.com"
+      />
       <HeroSlider />
       <AboutSection />
       <ServicesSection />
